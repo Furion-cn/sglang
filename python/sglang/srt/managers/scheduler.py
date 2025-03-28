b1af3b817c1985c95fab865a8ea473e2e764d87e
tarinkk <129432511+tarinkk@users.noreply.github.com>
2025-03-27 20:09:35 -0400

    Support (1 <= dp < tp) in the dp attention in DeepEP (#4770)
    Co-authored-by: Cheng Wan <cwan39@gatech.edu>

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -1279,7 +1279,7 @@ class Scheduler(
                 ret = self.running_batch if not self.running_batch.is_empty() else None
 
         # Handle DP attention
-        if self.server_args.enable_dp_attention:
+        if self.server_args.enable_dp_attention or self.server_args.enable_sp_layernorm:
             ret, _ = self.prepare_dp_attn_batch(ret)
 
         return ret
