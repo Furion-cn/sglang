22789f913929502508ab286f6a77a4d63b8de665
Mick <mickjagger19@icloud.com>
2025-04-12 12:46:58 +0800

    [VLM] Adopt fast image processor by default (#5065)

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -213,6 +213,9 @@ class ServerArgs:
     disaggregation_mode: str = "null"
     disaggregation_bootstrap_port: int = 8998
 
+    # multimodal
+    disable_fast_image_processor: bool = False
+
     def __post_init__(self):
         # Expert parallelism
         if self.enable_ep_moe:
@@ -1003,6 +1006,7 @@ class ServerArgs:
         )
         parser.add_argument(
             "--enable-llama4-multimodal",
+            default=ServerArgs.enable_llama4_multimodal,
             action="store_true",
             help="Enable the multimodal functionality for Llama-4.",
         )
@@ -1199,6 +1203,13 @@ class ServerArgs:
             type=int,
             default=ServerArgs.disaggregation_bootstrap_port,
             help="Bootstrap server port on the prefill server. Default is 8998.",
+        )
+
+        # Multimodal
+        parser.add_argument(
+            "--disable-fast-image-processor",
+            action="store_true",
+            help="Adopt base image processor instead of fast image processor.",
         )
 
     @classmethod
