fee9559bea507ae9e17dbf22f234206fd475a63f
xiaolin <xiaolin@minimaxi.com>
2025-04-18 15:19:29 +0800

    watch child signal

--- python/sglang/launch_server.py
+++ python/sglang/launch_server.py
@@ -6,11 +6,13 @@ import sys
 from sglang.srt.entrypoints.http_server import launch_server
 from sglang.srt.server_args import prepare_server_args
 from sglang.srt.utils import kill_process_tree
+from sglang.srt.managers.process_mamanger import setup_child_process_monitor
 
 if __name__ == "__main__":
     server_args = prepare_server_args(sys.argv[1:])
 
     try:
         launch_server(server_args)
+        setup_child_process_monitor()
     finally:
         kill_process_tree(os.getpid(), include_parent=False)
