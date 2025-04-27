473f55491762525a652f6c09d75c394d12f218f6
xiaolin <xiaolin@minimaxi.com>
2025-04-27 20:09:27 +0800

    feat

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -869,7 +869,7 @@ class DeepseekV2AttentionMLA(nn.Module):
                 q, latent_cache = self.fused_qkv_a_proj_with_mqa(hidden_states)[0].split(
                     [self.q_lora_rank, self.kv_lora_rank + self.qk_rope_head_dim], dim=-1
                 )
-                q = self.q_a_layernorm(q.contiguous())
+                q = self.q_a_layernorm(q)
                 q = self.q_b_proj(q)[0].view(-1, self.num_local_heads, self.qk_head_dim)
             else:
                 q = self.q_proj(hidden_states)[0].view(
