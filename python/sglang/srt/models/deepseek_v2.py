59557d2eae4d6e00f5f993fe61a980d6240d4a13
pc-new <pengchengneo@gmail.com>
2025-04-30 18:35:24 +0800

    expert load balance

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -1428,12 +1428,12 @@ class DeepseekV2DecoderLayer(nn.Module):
             and not forward_batch.forward_mode.is_idle()
             and hidden_states.shape[0] > 0
         ):
-            # router_logits: (num_tokens, n_experts)
-            # router_logits = self.mlp.gate(hidden_states)
-            router_logits = torch.rand(
-                    (hidden_states.shape[0], self.mlp.num_experts),
-                    device=hidden_states.device
-                ) 
+            router_logits: (num_tokens, n_experts)
+            router_logits = self.mlp.gate(hidden_states)
+            # router_logits = torch.rand(
+            #         (hidden_states.shape[0], self.mlp.num_experts),
+            #         device=hidden_states.device
+            #     ) 
             topk_weights, topk_idx = select_experts(
                 hidden_states=hidden_states,
                 router_logits=router_logits,
@@ -1449,17 +1449,17 @@ class DeepseekV2DecoderLayer(nn.Module):
             #     f"topk_weights.shape: {topk_weights.shape}"
             #     f"topk_idx.shape: {topk_idx.shape}"
             # )
-            # if topk_idx is not None and topk_idx.shape[0] > 0:
-            #     flat_topk = topk_idx.to("cpu").flatten().cpu().numpy()
-            #     for idx in flat_topk:
-            #         while len(expert_cnt) <= idx:
-            #             expert_cnt.append(0)
-            #         expert_cnt[idx] += 1
-            #     if len(expert_cnt) > 0 and sum(expert_cnt) % 100000 == 0:
-            #         expert_info = "\nExpert selection distribution:"
-            #         for i, cnt in enumerate(expert_cnt):
-            #             expert_info += f"Expert {i}: {cnt} selections,"
-            #         logger.debug("**************" + expert_info + "**************")
+            if topk_idx is not None and topk_idx.shape[0] > 0:
+                flat_topk = topk_idx.to("cpu").flatten().cpu().numpy()
+                for idx in flat_topk:
+                    while len(expert_cnt) <= idx:
+                        expert_cnt.append(0)
+                    expert_cnt[idx] += 1
+                if len(expert_cnt) > 0 and sum(expert_cnt) % 100000 == 0:
+                    expert_info = "\nExpert selection distribution:"
+                    for i, cnt in enumerate(expert_cnt):
+                        expert_info += f"Expert {i}: {cnt} selections,"
+                    logger.debug("**************" + expert_info + "**************")
         extra_args.update(
             {
                 MicroBatchOverlapExtraArgs.EXTRA_ARGS_TOPK_IDX_KEY: topk_idx,
