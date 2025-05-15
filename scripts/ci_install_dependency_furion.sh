859f701d596608bee983b3fa08deb06e0570cbb1
RamezesDong <donghouze666@outlook.com>
2025-05-15 17:13:39 +0800

    feat: add ci test

--- scripts/ci_install_dependency_furion.sh
+++ scripts/ci_install_dependency_furion.sh
@@ -9,28 +9,28 @@ bash "${SCRIPT_DIR}/killall_sglang.sh"
 pip install --upgrade pip
 
 # Clean up existing installations
-pip uninstall -y flashinfer flashinfer_python sgl-kernel sglang vllm
+pip uninstall -y sglang
 pip cache purge
 # rm -rf /root/.cache/flashinfer
 # rm -rf /usr/local/lib/python3.10/dist-packages/flashinfer*
 # rm -rf /usr/local/lib/python3.10/dist-packages/sgl_kernel*
 
 # Install the main package
-pip install -e "python[all]" --find-links https://flashinfer.ai/whl/cu124/torch2.5/flashinfer-python -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+pip install -e "python[all]" --find-links https://flashinfer.ai/whl/cu124/torch2.5/flashinfer-python 
 
 # Install additional dependencies
-pip install transformers==4.51.0 timm torchaudio==2.6.0 sentence_transformers accelerate peft pandas datasets mooncake-transfer-engine==0.3.0 -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+pip install transformers==4.51.0 timm torchaudio==2.6.0 sentence_transformers accelerate peft pandas datasets mooncake-transfer-engine==0.3.0
 
 
 # For compiling xgrammar kernels
-pip install cuda-python nvidia-cuda-nvrtc-cu12 -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+pip install cuda-python nvidia-cuda-nvrtc-cu12 
 
 # For lmms_evals evaluating MMMU
 git clone --branch v0.3.3 --depth 1 https://github.com/EvolvingLMMs-Lab/lmms-eval.git
-pip install -e lmms-eval/ -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+pip install -e lmms-eval/
 
 # Install FlashMLA for attention backend tests
-pip install git+https://github.com/deepseek-ai/FlashMLA.git -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+pip install git+https://github.com/deepseek-ai/FlashMLA.git
 
 # Install hf_xet
-pip install huggingface_hub[hf_xet] -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+pip install huggingface_hub[hf_xet]
