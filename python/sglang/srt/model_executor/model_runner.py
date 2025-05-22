7eb4535debe8c5f40625eb2d073b150caa25d10c
pc-new <pengchengneo@gmail.com>
2025-05-22 22:49:42 +0800

    fix

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -1157,8 +1157,10 @@ class ModelRunner:
                 pp_proxy_tensors=pp_proxy_tensors,
             )
             logger.info(f"run_cuda_graph replay use time: {time.time() - now}")
-            logger.info(f"run_cuda_graph replay ret:hidden_states{ret.hidden_states}")
-            logger.info(f"run_cuda_graph replay ret:next_token_logits{ret.next_token_logits}")
+            if ret.hidden_states is not None:
+                logger.info(f"run_cuda_graph replay ret:hidden_states{ret.hidden_states}")
+            if ret.next_token_logits is not None:
+                logger.info(f"run_cuda_graph replay ret:next_token_logits{ret.next_token_logits}")
         elif forward_batch.forward_mode.is_decode():
             now = time.time()
             logger.info(f"run_decode start_time{now}")
