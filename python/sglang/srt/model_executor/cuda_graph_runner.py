c467f01a3e558783e24a440cfc438f790b0d42c3
jimoosciuc <33337387+jimoosciuc@users.noreply.github.com>
2025-05-24 15:11:06 +0800

    Feat/rebase mask group gemm (#59)
    * fix: remove debug logs
    
    * fix: refactor masked group gemm kernel
    
    * fix: cuda graph build panic
    
    * refactor: normal group gemm triton kernel configs

--- python/sglang/srt/model_executor/cuda_graph_runner.py
+++ python/sglang/srt/model_executor/cuda_graph_runner.py
@@ -47,6 +47,13 @@ if TYPE_CHECKING:
 
 _is_hip = is_hip()
 
+# Detect whether the current forward pass is in capture mode
+is_capture_mode = False
+
+
+def get_is_capture_mode():
+    return is_capture_mode
+
 
 def _to_torch(model: torch.nn.Module, reverse: bool, num_tokens: int):
     for sub in model._modules.values():
@@ -294,17 +301,12 @@ class CudaGraphRunner:
 
     @contextmanager
     def model_capture_mode(self):
-        if hasattr(self.model_runner.model, "capture_mode"):
-            self.model_runner.model.capture_mode = True
-        if hasattr(self.model_runner.token_to_kv_pool, "capture_mode"):
-            self.model_runner.token_to_kv_pool.capture_mode = True
+        global is_capture_mode
+        is_capture_mode = True
 
         yield
 
-        if hasattr(self.model_runner.model, "capture_mode"):
-            self.model_runner.model.capture_mode = False
-        if hasattr(self.model_runner.token_to_kv_pool, "capture_mode"):
-            self.model_runner.token_to_kv_pool.capture_mode = False
+        is_capture_mode = False
 
     def can_run(self, forward_batch: ForwardBatch):
         if self.enable_dp_attention or self.enable_sp_layernorm:
