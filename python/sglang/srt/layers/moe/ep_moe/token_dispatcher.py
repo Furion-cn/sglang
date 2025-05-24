232276abb6a277f5b5ae3ebba90e56b9804b52f9
pc-new <pengchengneo@gmail.com>
2025-05-24 22:03:50 +0800

    fix bug

--- python/sglang/srt/layers/moe/ep_moe/token_dispatcher.py
+++ python/sglang/srt/layers/moe/ep_moe/token_dispatcher.py
@@ -96,7 +96,7 @@ class DeepEPBuffer:
             group,
             num_nvl_bytes,
             num_rdma_bytes,
-            low_latency_mode=deepep_mode.enable_low_latency(),
+            low_latency_mode=True,
             num_qps_per_rank=(max(num_experts // group.size(), Buffer.num_sms // 2)),
         )
         return cls._buffer
