58c76bc7839904cd9067bef4cc82c21bc43fa473
pc-new <pengchengneo@gmail.com>
2025-06-07 11:48:07 +0800

    fix bug

--- python/sglang/srt/managers/schedule_batch.py
+++ python/sglang/srt/managers/schedule_batch.py
@@ -891,6 +891,7 @@ class ScheduleBatch(ScheduleBatchDisaggregationDecodeMixin):
     # Enable custom logit processor
     enable_custom_logit_processor: bool = False
     schedule_batch_time: float = 0.0
+    num_prefill_tokens: int = 0
     # Whether to return hidden states
     return_hidden_states: bool = False
 
@@ -907,6 +908,7 @@ class ScheduleBatch(ScheduleBatchDisaggregationDecodeMixin):
         enable_custom_logit_processor: bool,
         chunked_req: Optional[Req] = None,
         schedule_batch_time: Optional[float] = 0.0,
+        num_prefill_tokens: Optional[int] = 0,
     ):
         return_logprob = any(req.return_logprob for req in reqs)
 
@@ -926,6 +928,7 @@ class ScheduleBatch(ScheduleBatchDisaggregationDecodeMixin):
             return_hidden_states=any(req.return_hidden_states for req in reqs),
             chunked_req=chunked_req,
             schedule_batch_time=schedule_batch_time,
+            num_prefill_tokens=num_prefill_tokens,
         )
 
     def batch_size(self):
