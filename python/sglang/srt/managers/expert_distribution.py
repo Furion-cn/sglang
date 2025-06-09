9de44264ffddad408404062680ffa33876a270d6
pc-new <pengchengneo@gmail.com>
2025-06-09 14:54:30 +0800

    epic0606 new debujg

--- python/sglang/srt/managers/expert_distribution.py
+++ python/sglang/srt/managers/expert_distribution.py
@@ -386,6 +386,8 @@ class _LayerBasedSinglePassGatherer(_SinglePassGatherer):
         self._objects_of_layer = {}
 
     def _on_layer_data(self, layer_idx: int, objects: List[int]):
+        if layer_idx is None:
+            return
         assert 0 <= layer_idx < self._expert_location_metadata.num_layers
         if layer_idx in self._objects_of_layer:
             self._objects_of_layer[layer_idx] = _list_sum(
--- python/sglang/srt/managers/schedule_batch.py
+++ python/sglang/srt/managers/schedule_batch.py
@@ -890,7 +890,8 @@ class ScheduleBatch(ScheduleBatchDisaggregationDecodeMixin):
 
     # Enable custom logit processor
     enable_custom_logit_processor: bool = False
-
+    schedule_batch_time: float = 0.0
+    num_prefill_tokens: int = 0
     # Whether to return hidden states
     return_hidden_states: bool = False
 
@@ -906,6 +907,8 @@ class ScheduleBatch(ScheduleBatchDisaggregationDecodeMixin):
         spec_algorithm: SpeculativeAlgorithm,
         enable_custom_logit_processor: bool,
         chunked_req: Optional[Req] = None,
+        schedule_batch_time: Optional[float] = 0.0,
+        num_prefill_tokens: Optional[int] = 0,
     ):
         return_logprob = any(req.return_logprob for req in reqs)
 
@@ -924,6 +927,8 @@ class ScheduleBatch(ScheduleBatchDisaggregationDecodeMixin):
             enable_custom_logit_processor=enable_custom_logit_processor,
             return_hidden_states=any(req.return_hidden_states for req in reqs),
             chunked_req=chunked_req,
+            schedule_batch_time=schedule_batch_time,
+            num_prefill_tokens=num_prefill_tokens,
         )
 
     def batch_size(self):
