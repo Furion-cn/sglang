f7f77ec3f1cc6f58175bea22028b1adf81c5d74a
aolemila <aolemilaluo@gmail.com>
2025-06-18 13:53:31 +0000

    fix some err

--- python/sglang/srt/jax/test/test_qwen.py
+++ python/sglang/srt/jax/test/test_qwen.py
@@ -3,15 +3,15 @@ from sglang.srt.jax.models.qwen import QWenMLP
 from jax import numpy as jnp
 import jax
 
+
 class TestQwen(unittest.TestCase):
-    def test_qwen_mlp():
-        MLP=QWenMLP(
+    def test_qwen_mlp(self):
+        MLP = QWenMLP(
             hidden_size=4096,
             intermediate_size=22016,
             hidden_act="silu",
             quant_config=None,
         )
-        hidden_states=jnp.ones((1,4096))
-        jax.sharding.with_sharding_constraint(hidden_states,jax.sharding.Mesh(('data',None)))
-        output=MLP(hidden_states)
-        assert output.shape==(1,4096)
+        hidden_states = jnp.ones((1, 4096))
+        output = MLP(hidden_states)
+        assert output.shape == (1, 4096)
