5223975e7254f2b4fb63f971fd71aef3d7525ccf
aolemila <aolemilaluo@gmail.com>
2025-06-19 21:09:25 +0800

    test/add partition and correctness check for mlp and logits_process (#97)
    * add correctness and sharding in test
    
    * fix conflicts

--- python/sglang/srt/jax/layers/logits_processor.py
+++ python/sglang/srt/jax/layers/logits_processor.py
@@ -3,6 +3,14 @@ from typing import Optional, Sequence
 
 import jax
 from flax import nnx
+from transformers import PretrainedConfig
+
+
+from sglang.srt.model_executor.forward_batch_info import ForwardBatch
+from jax.lax import with_sharding_constraint
+from jax.sharding import PartitionSpec
+from jax import numpy as jnp
+from flax.typing import Sharding
 
 
 @dataclasses.dataclass
@@ -19,14 +27,18 @@ class LogitsProcessor(nnx.Module):
         vocab_size: int,
         kernel_axes: Optional[Sequence[str]] = None,
         rngs: nnx.Rngs = None,
+
     ):
-        vocab_size = ((vocab_size + 63) // 64) * 64
+        if kernel_axes is not None:
+            kernel_axes = tuple(axis for axis in kernel_axes if axis is not None)
+            kernel_init=nnx.with_partitioning(nnx.initializers.lecun_normal(),kernel_axes)
+        else:
+            kernel_init=nnx.initializers.lecun_normal()
 
         self.lm_head = nnx.Linear(
             hidden_size,
             vocab_size,
-            kernel_init=nnx.with_partitioning(
-                nnx.initializers.lecun_normal(), kernel_axes),
+            kernel_init=kernel_init,
             use_bias=False,
             rngs=rngs
         )
@@ -35,5 +47,7 @@ class LogitsProcessor(nnx.Module):
         self,
         hidden_states: jax.Array,
     ) -> LogitsProcessorOutput:
+        hidden_states = with_sharding_constraint(
+            hidden_states, PartitionSpec('data', None))
         logits = self.lm_head(hidden_states)
         return LogitsProcessorOutput(logits=logits)
