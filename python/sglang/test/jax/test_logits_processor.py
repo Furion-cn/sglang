b849b980ee172b8e844aee664cfe816d64fb313b
aolemila <aolemilaluo@gmail.com>
2025-06-20 14:26:31 +0800

    fix error in rng.params with rng.params() in Embed and update codes w… (#102)
    * fix error in rng.params with rng.params() in Embed and update codes with Embed and pass test
    
    * use transpose
    
    * shape

--- python/sglang/test/jax/test_logits_processor.py
+++ python/sglang/test/jax/test_logits_processor.py
@@ -7,26 +7,39 @@ from sglang.srt.jax.layers.logits_processor import LogitsProcessor
 from jax.sharding import Mesh
 import numpy as np
 import os
+from sglang.srt.jax.layers.embeddings import Embed
+from transformers import PretrainedConfig
 
 os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=4'
 
 
-mesh = Mesh(devices=np.array(jax.devices()).reshape(2, -1),
-            axis_names=('data', 'model'))
+mesh = Mesh(devices=np.array(jax.devices()).reshape(2, -1, 1, 1),
+            axis_names=('data', 'model', 'vocab', 'embed'))
 
 
-hidden_size = 4
-vocab_size = 4
-batch_size = 2
+hidden_size = 4096
+vocab_size = 151936
+batch_size = 256
 
 
 class TestLogitsProcessor(unittest.TestCase):
     def test_logits_processor(self):
-        def create_sharded_model():
+        def create_sharded_lm_head():
             # Unsharded at this moment.
-            model = LogitsProcessor(
-                hidden_size=hidden_size,
-                vocab_size=vocab_size,
+            model = Embed(
+                config=PretrainedConfig(
+                    vocab_size=vocab_size,
+                    hidden_size=hidden_size,
+                    num_hidden_layers=12,
+                    num_attention_heads=16,
+                    intermediate_size=4096,
+                    max_position_embeddings=1024,
+                    rope_theta=10000,
+                    layer_norm_epsilon=1e-5,
+                    weight_dtype=jnp.float32,
+                ),
+                num_embeddings=vocab_size,
+                features=hidden_size,
                 rngs=nnx.Rngs(0)
             )
             # The model's state, a pure pytree.
@@ -39,17 +52,19 @@ class TestLogitsProcessor(unittest.TestCase):
             return model
 
         def _ref_logits_processor(hidden_states, weight):
-            return jnp.dot(hidden_states, weight, precision=jax.lax.Precision.HIGHEST, preferred_element_type=jnp.float32)
+            return jnp.dot(hidden_states[:, -1:, :], weight, precision=jax.lax.Precision.DEFAULT, preferred_element_type=jnp.float32)
 
         with mesh:
-            sharded_model = create_sharded_model()
-            hidden_states = jnp.ones((batch_size, hidden_size))
-            output = sharded_model(hidden_states)
+            sharded_lm_head = create_sharded_lm_head()
+            hidden_states = jax.random.randint(
+                nnx.Rngs(0).params(), (batch_size, 3, hidden_size), 0, vocab_size)
+            logits_processor = LogitsProcessor()
+            output = logits_processor(hidden_states, sharded_lm_head)
             ref_output = _ref_logits_processor(
-                hidden_states, sharded_model.lm_head.kernel.value)
+                hidden_states,  jnp.asarray(sharded_lm_head.weight.value, jnp.bfloat16).T)
 
             # check shape
-            assert output.logits.shape == (batch_size, vocab_size)
+            assert output.logits.shape == (batch_size, 1, vocab_size)
             # check correctness
             assert jnp.allclose(output.logits, ref_output)
             print(f"✓ Logits processor output is correct!")
