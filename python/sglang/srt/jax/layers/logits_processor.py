3347436c18a48cccd90d7e4cb62489d140e6b5cf
aolemila <aolemilaluo@gmail.com>
2025-07-02 14:37:12 +0800

    Feat/support jit for MLP and logitsProcessor(#143)
    * MLP and LogitsProccessor support jax.jit

--- python/sglang/srt/jax/layers/logits_processor.py
+++ python/sglang/srt/jax/layers/logits_processor.py
@@ -6,6 +6,8 @@ from flax import nnx
 
 from sglang.srt.jax.layers.embeddings import Embed
 from sglang.srt.jax.model_executor.forward_batch_info import ForwardBatch, ForwardMode
+from functools import partial
+from flax.typing import PromoteDtypeFn
 
 
 @dataclasses.dataclass
@@ -26,21 +28,82 @@ class LogitsProcessor(nnx.Module):
         lm_head: Embed,
         forward_batch: ForwardBatch,
     ) -> LogitsProcessorOutput:
-        # hidden_states = with_sharding_constraint(
-        #    hidden_states, PartitionSpec('data', None))
-        # hidden_states.shape = [total_tokens, hidden_size]
-
-        # Extract the last token of each sequence based on seq_lens
-        # forward_batch.extend_start_loc gives start position of each sequence
-        # forward_batch.seq_lens gives length of each sequence
         if forward_batch.forward_mode == ForwardMode.EXTEND:
-            last_token_indices = forward_batch.extend_start_loc + forward_batch.seq_lens - 1
+            logits=_logits_processor_forward_extend(
+                hidden_states,
+                forward_batch.extend_start_loc,
+                forward_batch.seq_lens,
+                lm_head.promote_dtype,
+                lm_head.embedding.value,
+                lm_head.dtype,
+                self.vocab_size,
+            )
         else:
-            last_token_indices = jnp.arange(forward_batch.batch_size)
-        # Shape: [batch_size, hidden_size]
-        last_hidden_states = hidden_states[last_token_indices]
-
-        logits = lm_head.attend(last_hidden_states)
-        logits = logits[:,
-                        :self.vocab_size] if logits.ndim > 1 else logits[:self.vocab_size]
+            logits=_logits_processor_forward_decode(
+                hidden_states,
+                lm_head.promote_dtype,
+                lm_head.embedding.value,
+                forward_batch.batch_size,
+                lm_head.dtype,
+                self.vocab_size,
+            )
         return LogitsProcessorOutput(next_token_logits=logits)
+
+@partial(jax.jit,static_argnums=(3,5,6))
+def _logits_processor_forward_extend(
+    hidden_states:jax.Array,
+    extend_start_loc:jax.Array,
+    seq_lens: jax.Array,
+    promote_dtype:PromoteDtypeFn,
+    embedding:jax.Array,
+    dtype:jnp.dtype,
+    vocab_size:int,
+    ):
+    last_token_indices = extend_start_loc + seq_lens - 1
+    # Shape: [batch_size, hidden_size]
+    last_hidden_states = hidden_states[last_token_indices]
+
+    return _lm_head_forward(
+        last_hidden_states,
+        embedding,
+        promote_dtype,
+        dtype,
+        vocab_size,
+    )
+
+@partial(jax.jit,static_argnums=(1,3,4,5))
+def _logits_processor_forward_decode(
+    hidden_states:jax.Array,
+    promote_dtype:PromoteDtypeFn,
+    embedding:jax.Array,
+    batch_size:int,
+    dtype:jnp.dtype,
+    vocab_size:int,
+    ):
+    last_token_indices = jnp.arange(batch_size)
+    # Shape: [batch_size, hidden_size]
+    last_hidden_states = hidden_states[last_token_indices]
+    return _lm_head_forward(
+        last_hidden_states,
+        embedding,
+        promote_dtype,
+        dtype,
+        vocab_size,
+    )
+
+@partial(jax.jit,static_argnums=(2,3,4))
+def _lm_head_forward(
+    last_hidden_states:jax.Array,
+    embedding:jax.Array,
+    promote_dtype:PromoteDtypeFn, 
+    dtype:jnp.dtype,
+    vocab_size:int,
+    ):
+    last_hidden_states, embedding = promote_dtype(
+            (last_hidden_states, embedding), dtype=dtype
+        )
+    logits=jnp.dot(last_hidden_states, embedding.T)
+
+    logits = logits[:,
+                    :vocab_size] if logits.ndim > 1 else logits[:vocab_size]
+    return logits
