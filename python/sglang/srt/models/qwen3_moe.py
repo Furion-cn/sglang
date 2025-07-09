872e5b78c3c755dde4dcdc8875eac608666792f3
yanami_anna <yanami_anna@gmail.com>
2025-07-10 00:24:36 +0800

    qwen3 moe

--- python/sglang/srt/models/qwen3_moe.py
+++ python/sglang/srt/models/qwen3_moe.py
@@ -199,6 +199,7 @@ class Qwen3MoeSparseMoeBlock(nn.Module):
         
         if self.tp_size > 1:
             final_hidden_states = tensor_model_parallel_all_reduce(final_hidden_states)
+            logger.info(f"layer_id: {self.layer_id}, ============tp_size==============: {self.tp_size}, ==============moe_after_all_reduce============: {final_hidden_states}, min: {final_hidden_states.min()}, max: {final_hidden_states.max()}, mean: {final_hidden_states.mean()}, std: {final_hidden_states.std()}")
             global_tracer.print(final_hidden_states, f"moe_after_all_reduce", f"moe_combine_layer_id_{self.layer_id}")
 
         logger.info(f"layer_id: {self.layer_id}, ==========moe_final_output===========: {final_hidden_states}, min: {final_hidden_states.min()}, max: {final_hidden_states.max()}, mean: {final_hidden_states.mean()}, std: {final_hidden_states.std()}")        
