db46a6884ded397adbab02a884841ac6a86557de
Yanami Anna <24688231+xcode03@users.noreply.github.com>
2025-07-10 21:27:49 +0800

    Feat/qwen3 moe ut2 (#156)
    * support qwen3
    
    * Feat/jax moe (#120)
    
    * jax moe init
    
    * jax moe init
    
    * jax moe init
    
    * jax moe init
    
    ---------
    
    Co-authored-by: yanami_anna <yanami_anna@gmail.com>
    
    * add gqa
    
    * Feat/qwen3 moe ut (#147)
    
    * qwen3 moe ut
    
    * qwen3 moe
    
    ---------
    
    Co-authored-by: yanami_anna <yanami_anna@gmail.com>
    
    * remove some temp
    
    ---------
    
    Co-authored-by: RamezesDong <donghouze666@outlook.com>
    Co-authored-by: yanami_anna <yanami_anna@gmail.com>

--- python/sglang/srt/jax/models/qwen.py
+++ python/sglang/srt/jax/models/qwen.py
@@ -112,7 +112,6 @@ class QWenAttention(nnx.Module):
             rotary_dim=head_size,
             max_position_embeddings=max_position_embeddings,
             base=rope_theta,
-            is_neox_style=False,
             dtype=jnp.bfloat16,
         )
 
