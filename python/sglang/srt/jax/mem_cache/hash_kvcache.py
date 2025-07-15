2a593f5d3a0e2f965da5e8c1a2b7475d7bd3669f
leos <kexikang@gmail.com>
2025-07-15 15:57:21 +0800

    feat: in place update kv cache

--- python/sglang/srt/jax/mem_cache/hash_kvcache.py
+++ python/sglang/srt/jax/mem_cache/hash_kvcache.py
@@ -1,5 +1,7 @@
 from functools import partial
 from typing import Dict, Tuple
+from jax.experimental import pallas as pl
+from jax.experimental.pallas import tpu as pltpu
 
 import jax
 import jax.numpy as jnp
@@ -54,6 +56,166 @@ class ReqToHashKVCachePool(KVCache):
         )
 
 
+def cdiv(a: int, b: int) -> int:
+    """Ceiling division."""
+    return -(a // -b)
+
+
+def _kv_cache_update_kernel(
+    # Prefetch
+    slices_ref,  # [3, padded_num_slices], list of (kv_cache_start,
+    # new_kv_start, slice_len)
+    # Input
+    new_kv_hbm_ref,  # [num_tokens, num_combined_kv_heads, head_dim]
+    kv_cache_hbm_ref,  # [total_num_pages * page_size, num_combined_kv_heads,
+    # head_dim]
+    # Output
+    _,  # [total_num_pages * page_size, num_combined_kv_heads, head_dim]
+    # Scratch
+    scratch,  # [num_slices_per_block, page_size, num_combined_kv_heads,
+    # head_dim]
+    sem,
+):
+    async_copies = []
+    block_idx = pl.program_id(0)
+    num_slices_per_block = scratch.shape[0]
+    # Copy from new_kv_hbm_ref to scratch
+    for i in range(num_slices_per_block):
+        offset_i = i + block_idx * num_slices_per_block
+        new_kv_start = slices_ref[1, offset_i]
+        length = slices_ref[2, offset_i]
+        async_copy = pltpu.make_async_copy(
+            new_kv_hbm_ref.at[pl.ds(new_kv_start, length), ...],
+            scratch.at[i, pl.ds(0, length), ...],
+            sem,
+        )
+        async_copy.start()
+        async_copies.append(async_copy)
+
+    for async_copy in async_copies:
+        async_copy.wait()
+
+    # Copy from scratch to kv_cache_hbm_ref
+    async_copies.clear()
+    for i in range(num_slices_per_block):
+        offset_i = i + block_idx * num_slices_per_block
+        kv_cache_start = slices_ref[0, offset_i]
+        length = slices_ref[2, offset_i]
+        async_copy = pltpu.make_async_copy(
+            scratch.at[i, pl.ds(0, length), ...],
+            kv_cache_hbm_ref.at[pl.ds(kv_cache_start, length), ...],
+            sem,
+        )
+        async_copy.start()
+        async_copies.append(async_copy)
+    for async_copy in async_copies:
+        async_copy.wait()
+
+
+@partial(
+    jax.jit,
+    static_argnames=["page_size", "num_slices_per_block"],
+)
+def kv_cache_update(
+    new_kv: jax.Array,  # [total_num_token, num_combined_kv_heads, head_dim]
+    # [3, slices], list of (kv_cache_start, new_kv_start, slice_len)
+    slices: jax.Array,
+    # [total_num_pages * page_size, num_combined_kv_heads, head_dim]
+    kv_cache: jax.Array,
+    num_kv_update_slices: jax.Array,  # [1]
+    *,
+    page_size: int = 1024,
+    num_slices_per_block: int = 8,
+):
+    assert slices.shape[1] % num_slices_per_block == 0, f"slices.shape[1]={slices.shape[1]} is not divisible by num_slices_per_block={num_slices_per_block}"
+    _, num_combined_kv_heads, head_dim = new_kv.shape
+    assert kv_cache.shape[1] == num_combined_kv_heads, f"kv_cache.shape[1]={kv_cache.shape[1]} is not equal to num_combined_kv_heads={num_combined_kv_heads}"
+    assert kv_cache.shape[2] == head_dim, f"kv_cache.shape[2]={kv_cache.shape[2]} is not equal to head_dim={head_dim}"
+    assert head_dim % 128 == 0, f"head_dim={head_dim} is not divisible by 128"
+    # TODO: Add dynamic check to make sure that the all the slice lengths are
+    # smaller or equal to page_size
+
+    in_specs = [
+        pl.BlockSpec(memory_space=pltpu.TPUMemorySpace.ANY),
+        pl.BlockSpec(memory_space=pltpu.TPUMemorySpace.ANY),
+    ]
+
+    out_specs = [pl.BlockSpec(memory_space=pltpu.TPUMemorySpace.ANY)]
+    out_shape = [jax.ShapeDtypeStruct(kv_cache.shape, dtype=kv_cache.dtype)]
+
+    scalar_prefetches = [slices]
+    scratch = pltpu.VMEM(
+        (num_slices_per_block, page_size, num_combined_kv_heads, head_dim),
+        new_kv.dtype,
+    )
+
+    scratch_shapes = [
+        scratch,
+        pltpu.SemaphoreType.DMA,
+    ]
+
+    kernel = pl.pallas_call(
+        _kv_cache_update_kernel,
+        grid_spec=pltpu.PrefetchScalarGridSpec(
+            num_scalar_prefetch=len(scalar_prefetches),
+            in_specs=in_specs,
+            out_specs=out_specs,
+            grid=(cdiv(num_kv_update_slices[0], num_slices_per_block), ),
+            scratch_shapes=scratch_shapes,
+        ),
+        out_shape=out_shape,
+        input_output_aliases={len(scalar_prefetches) + 1: 0},
+    )
+
+    return kernel(*scalar_prefetches, new_kv, kv_cache)[0]
+
+
+def _get_slot_mapping(
+    num_slices_per_block: int,
+    kv_cache_start_loc: jax.Array,
+    new_kv_start_loc: jax.Array,
+    slice_lens: jax.Array,
+):
+    slot_mapping = jnp.stack(
+        [kv_cache_start_loc, new_kv_start_loc, slice_lens], axis=1)
+    padded_size = (slot_mapping.shape[0] + num_slices_per_block -
+                   1) // num_slices_per_block * num_slices_per_block
+    slot_mapping = jnp.pad(slot_mapping,
+                           [[0, padded_size - slot_mapping.shape[0]], [0, 0]],
+                           constant_values=0)
+    slot_mapping = jnp.transpose(slot_mapping)
+    return slot_mapping
+
+
+VME_SIZE = 32 * 1024 * 1024 # 32MB
+NUM_SLICES_PER_BLOCK = 4
+PAGE_SIZE = 1024
+
+
+@jax.jit
+def update_kv_cache(
+    k: jax.Array,          # padding key (batch_size)
+    v: jax.Array,          # padding value
+    k_cache: jax.Array,
+    v_cache: jax.Array,
+    seq_lens: jax.Array,   # (batch_size, )
+    kv_start_loc: jax.Array,
+    kv_cache_start_loc: jax.Array,  # (batch_size, )
+):
+    batch_size = seq_lens.shape[0]
+    num_kv_update_slices = jnp.array([batch_size], dtype=jnp.int32)
+    get_slot_mapping = partial(
+        _get_slot_mapping, kv_cache_start_loc=kv_cache_start_loc, new_kv_start_loc=kv_start_loc, slice_lens=seq_lens)
+    slot_mapping = get_slot_mapping(NUM_SLICES_PER_BLOCK)
+
+    k_cache = kv_cache_update(k, slot_mapping, k_cache, num_kv_update_slices,
+                    num_slices_per_block=NUM_SLICES_PER_BLOCK, page_size=PAGE_SIZE)
+    v_cache = kv_cache_update(v, slot_mapping, v_cache, num_kv_update_slices,
+                    num_slices_per_block=NUM_SLICES_PER_BLOCK, page_size=PAGE_SIZE)
+    return k_cache, v_cache
+
+
+
 @partial(jax.jit, static_argnames=["layer_id"])
 def get_kv_buffer(layer_id: int, k_cache: jax.Array, v_cache: jax.Array) -> Tuple[jax.Array, jax.Array]:
     return k_cache[layer_id], v_cache[layer_id]
@@ -70,7 +232,7 @@ def set_kv_cache(
 ) -> Tuple[jax.Array, jax.Array]:
     assert loc.shape[0] == k.shape[0] == v.shape[0], "Batch size mismatch"
 
-    k_cache = k_cache.at[layer_id, loc].set(k)
+    k_cache = v_cache.at[layer_id, loc].set(k)
     v_cache = v_cache.at[layer_id, loc].set(v)
 
     return k_cache, v_cache
---
+++ python/sglang/test/jax/test_kv_cache.py
@@ -0,0 +1,101 @@
+import unittest
+
+import jax
+import jax.numpy as jnp
+from sglang.test.test_utils import CustomTestCase
+import random
+from sglang.srt.jax.mem_cache.hash_kvcache import update_kv_cache
+
+
+class TestKVCache(CustomTestCase):
+    """Test cases for the Attention layer."""
+
+    def setUp(self):
+        if not jax.devices():
+            self.skipTest("JAX not available")
+
+        # PyTorch comparison test parameters
+        try:
+            import torch
+            self.pytorch_available = True
+        except ImportError:
+            self.pytorch_available = False
+
+        self.max_seq_len = 16
+        self.num_heads = 2
+        self.head_dim = 128
+        self.batch_size = 2
+        self.layer_num = 2
+
+    def generate_test_data(self, layer_idx: int, is_prefill: bool):
+        max_total_len_of_one_layer = self.batch_size*self.max_seq_len
+        kv_cache_of_one_layer = jnp.zeros(
+            (max_total_len_of_one_layer, self.num_heads, self.head_dim), dtype=jnp.bfloat16)
+        self.k_cache = jnp.concatenate(
+            [kv_cache_of_one_layer] * self.layer_num, axis=0)
+        self.v_cache = jnp.concatenate(
+            [kv_cache_of_one_layer] * self.layer_num, axis=0)
+
+        if is_prefill:
+            self.k = jax.random.uniform(
+                jax.random.PRNGKey(42), (self.batch_size*self.max_seq_len, self.num_heads, self.head_dim), dtype=jnp.bfloat16)
+            self.v = jax.random.uniform(
+                jax.random.PRNGKey(42), (self.batch_size*self.max_seq_len, self.num_heads, self.head_dim), dtype=jnp.bfloat16)
+            self.seq_lens = jax.random.randint(
+                jax.random.PRNGKey(42), (self.batch_size,), 1, self.max_seq_len+1, dtype=jnp.int32)
+            self.kv_start_loc = jnp.arange(
+                self.batch_size, dtype=jnp.int32) * self.max_seq_len
+        else:
+            self.k = jax.random.uniform(
+                jax.random.PRNGKey(42), (self.batch_size, self.num_heads, self.head_dim), dtype=jnp.bfloat16)
+            self.v = jax.random.uniform(
+                jax.random.PRNGKey(42), (self.batch_size, self.num_heads, self.head_dim), dtype=jnp.bfloat16)
+            self.seq_lens = jnp.ones((self.batch_size,), dtype=jnp.int32)
+            self.kv_start_loc = jnp.arange(
+                self.batch_size, dtype=jnp.int32)
+
+        max_real_seq_len = jnp.max(self.seq_lens)
+        max_offset = self.max_seq_len - max_real_seq_len
+        self.kv_cache_start_loc = max_total_len_of_one_layer*layer_idx + (jnp.arange(
+            self.batch_size, dtype=jnp.int32) * self.max_seq_len + jax.random.randint(jax.random.PRNGKey(42), (self.batch_size,), 0, max_offset, dtype=jnp.int32))
+
+        return self.k, self.v, self.k_cache, self.v_cache, self.kv_cache_start_loc, self.seq_lens, self.kv_start_loc
+
+    def expected_at_set_update_kv_cache(self, k, v, k_cache, v_cache, kv_cache_start_loc, k_seq_lens, k_start_loc):
+        batch_size = k_seq_lens.shape[0]
+        for i in range(batch_size):
+            seq_len = k_seq_lens[i]
+            cache_loc = jnp.arange(
+                seq_len, dtype=jnp.int32) + kv_cache_start_loc[i]
+            k_cache = k_cache.at[cache_loc].set(k[k_start_loc[i]:k_start_loc[i]+seq_len, :, :])
+            v_cache = v_cache.at[cache_loc].set(v[k_start_loc[i]:k_start_loc[i]+seq_len, :, :])
+        return k_cache, v_cache
+
+    def test_kv_cache_update_prefill(self):
+        test_loop = random.randint(1, self.layer_num)
+        for layer_idx in range(test_loop):
+            # test_output
+            k, v, k_cache, v_cache, kv_cache_start_loc, k_seq_lens, k_start_loc = self.generate_test_data(
+                layer_idx, is_prefill=True)
+            k_cache, v_cache = update_kv_cache(k, v, k_cache, v_cache, k_seq_lens, k_start_loc, kv_cache_start_loc)
+            # expected data
+            expected_k_cache, expected_v_cache = self.expected_at_set_update_kv_cache(k, v, k_cache, v_cache, kv_cache_start_loc, k_seq_lens, k_start_loc)
+            self.assertTrue(jnp.allclose(k_cache, expected_k_cache))
+            self.assertTrue(jnp.allclose(v_cache, expected_v_cache))
+
+    def test_kv_cache_update_decode(self):
+        test_loop = random.randint(1, self.layer_num)
+        for layer_idx in range(test_loop):
+            # test_output
+            k, v, k_cache, v_cache, kv_cache_start_loc, k_seq_lens, k_start_loc = self.generate_test_data(
+                layer_idx, is_prefill=False)
+            k_cache, v_cache = update_kv_cache(k, v, k_cache, v_cache, k_seq_lens, k_start_loc, kv_cache_start_loc)
+            # expected data
+            expected_k_cache, expected_v_cache = self.expected_at_set_update_kv_cache(
+                k, v, k_cache, v_cache, kv_cache_start_loc, k_seq_lens, k_start_loc)
+            self.assertTrue(jnp.allclose(k_cache, expected_k_cache))
+            self.assertTrue(jnp.allclose(v_cache, expected_v_cache))
+
+
+if __name__ == "__main__":
+    unittest.main()
