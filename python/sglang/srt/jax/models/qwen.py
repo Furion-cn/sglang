d2d024ea638a4fe2b06b1fa480e8f761736cdb8c
RamezesDong <donghouze666@outlook.com>
2025-07-15 17:51:05 +0800

    merge test

--- python/sglang/srt/jax/models/qwen.py
+++ python/sglang/srt/jax/models/qwen.py
@@ -136,11 +136,11 @@ class QWenAttention(nnx.Module):
         qkv, _ = self.c_attn(hidden_states)
         q, k, v = jnp.split(qkv, 3, axis=-1)
 
-        jax.debug.print("layer {id} device {dev_id}/{num_dev} q local shape: {shape}",
+        jax.debug.print("layer {id} q local shape: {shape}",
                         id=layer_id,  shape=q.shape)
-        jax.debug.print("layer {id} device {dev_id}/{num_dev} k local shape: {shape}",
+        jax.debug.print("layer {id} k local shape: {shape}",
                         id=layer_id, shape=k.shape)
-        jax.debug.print("layer {id} device {dev_id}/{num_dev} v local shape: {shape}",
+        jax.debug.print("layer {id} v local shape: {shape}",
                         id=layer_id, shape=v.shape)
 
         q, k = self.rotary_emb(positions, q, k)
