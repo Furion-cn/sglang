2874436b3367c87966a9af9e93759e3ec73db0e7
RamezesDong <donghouze666@outlook.com>
2025-07-17 19:42:31 +0800

    debug qkv

--- python/sglang/test/jax/test_qwen3_model.py
+++ python/sglang/test/jax/test_qwen3_model.py
@@ -36,7 +36,7 @@ class TestQwen3DenseModel(unittest.TestCase):
 
     def setUp(self):
         self.mesh = create_device_mesh(
-            ici_parallelism=[-1, 1, 1, 1], dcn_parallelism=[1, 1, 1, 1])
+            ici_parallelism=[-1, 2, 1, 1], dcn_parallelism=[1, 1, 1, 1])
         # Model path for local model and tokenizer
         self.test_model_path = os.environ.get(
             'MODEL_PATH', 'Qwen/Qwen3-8B')  # Default to HuggingFace
